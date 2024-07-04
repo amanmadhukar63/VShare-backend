44c39ac94d856de3e21a7b7cd3d17162091063db
Aman Personel <aman@Amans-MacBook-Air.local>
2024-07-05 01:00:56 +0530

    middlewares

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 // require('dotenv').config({path:'./env'})
 // import dotenv from 'dotenv';
 import connectDB from "./db/db.js";
+import app from "./app.js";
 
-connectDB();
+const port=process.env.PORT || 3000;
+
+connectDB()
+.then(()=>{
+    app.listen(port,()=>{
+        console.log('Server is running on port',port);
+    })
+})
+.catch((err)=>{console.log('MongoDB connection failed',err)})
