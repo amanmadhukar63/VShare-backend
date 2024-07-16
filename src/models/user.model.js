99d059a6397c7eba8ba70e2a72530846de4f05f4
Aman Personel <aman@Amans-MacBook-Air.local>
2024-07-16 22:48:35 +0530

    added access and refresh token

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose, {Schema} from "mongoose";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 const userSchema = new Schema({
     username:{
@@ -51,6 +52,32 @@ userSchema.pre("save", async function (next){
 
 userSchema.methods.isPasswordCorrect= async function (password){
     return await bcrypt.compare(password,this.password);
+}
+
+userSchema.methods.generateAccessToken= async function (){
+    return await jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    );
+}
+
+userSchema.methods.generateRefreshToken = async function (){
+    return await jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        }
+    );
 }
 
 export default User = mongoose.model("User",userSchema);
