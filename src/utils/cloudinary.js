b498e0cec40cc06dcb4cc04c6f8c05ff1410975a
Aman Personel <aman@Amans-MacBook-Air.local>
2024-07-17 23:10:11 +0530

    project setup complete

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import { v2 as cloudinary } from "cloudinary";
+import { log } from "console";
+import fs from 'fs';
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: process.env.CLOUD_NAME, 
+    api_key: process.env.CLOUDINARY_API_KEY, 
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+// uploading...
+const uploadOnCloudinary = async function(localFilePath){
+    try {
+        const uploadResult = await cloudinary.uploader.upload(
+           localFilePath, 
+           {
+               resource_type: "auto",
+           }
+        );
+        console.log('File uploaded successfully on cloudinary-',uploadResult.url);
+        return uploadResult;
+    } catch (error) {
+        fs.unlinkSync(localFilePath);
+        console.log('File upload failed, file removed from server');
+        return null;
+    }
+}
+
+export {uploadOnCloudinary};
